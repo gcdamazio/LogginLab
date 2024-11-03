4040d3abe5eee0af6c961abb1445e50ffcbd9641
gcdamazio <damaziogabrielle@gmail.com>
2024-11-03 09:49:35 -0500

    DONE

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,7 +25,7 @@ public class LogginLabTest {
 
         for (Integer i = 1; i <= finalLimit; i++) {
             if (lab.thresholdExceeds(i)) {
-                logger.log(Level.INFO, "Threshold not reached! It is "+i);
+                logger.log(Level.INFO, "Threshold not reached! It is " + i);
                 assertTrue(lab.thresholdExceeds(i));
             } else {
                 logger.log(Level.INFO, "Threshold finally reached!");
@@ -31,4 +33,24 @@ public class LogginLabTest {
             }
         }
     }
-}
+
+        @Test
+        public void testThresholdReached() {
+            LogginLab lab = new LogginLab();
+
+            lab.setThreshold(10);
+
+            // Test  limit > threshold
+            assertTrue(lab.thresholdReached(15));
+
+            // Test limit = threshold
+            assertTrue(lab.thresholdReached(10));
+
+            // Test limit < threshold
+            assertFalse(lab.thresholdReached(5));
+        }
+    }
+
+
+
+
